4d65ed92d488ee362c6b5a62f63a5f322df4dbab
Adam French <adam.a.french@outlook.com>
2024-11-07 15:30:41 +0000

    starting app

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # data-vis
+Welcome to our data visualization project!
+
+# Useful matplotlib functions
+- Plot a scatter `ax.scatter(month, attendance)`
+- Plot a heatmap `ax.imshow(arr_grid)`
--- main.py
+++ main.py
@@ -1,62 +1,86 @@
-from tkinter import * 
-from matplotlib.figure import Figure 
-from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg,  
-NavigationToolbar2Tk) 
-  
-# plot function is created for  
-# plotting the graph in  
-# tkinter window 
-def plot(): 
-  
-    # the figure that will contain the plot 
-    fig = Figure(figsize = (5, 5), 
-                 dpi = 100) 
-  
-    # list of squares 
-    y = [i**2 for i in range(101)] 
-  
-    # adding the subplot 
-    plot1 = fig.add_subplot(111) 
-  
-    # plotting the graph 
-    plot1.plot(y) 
-  
-    # creating the Tkinter canvas 
-    # containing the Matplotlib figure 
-    canvas = FigureCanvasTkAgg(fig, 
-                               master = window)   
-    canvas.draw() 
-  
-    # placing the canvas on the Tkinter window 
-    canvas.get_tk_widget().pack() 
-  
-    # creating the Matplotlib toolbar 
-    toolbar = NavigationToolbar2Tk(canvas, 
-                                   window) 
-    toolbar.update() 
-  
-    # placing the toolbar on the Tkinter window 
-    canvas.get_tk_widget().pack() 
-  
-# the main Tkinter window 
-window = Tk() 
-  
-# setting the title  
-window.title('Plotting in Tkinter') 
-  
-# dimensions of the main window 
-window.geometry("500x500") 
-  
-# button that displays the plot 
-plot_button = Button(master = window,  
-                     command = plot, 
-                     height = 2,  
-                     width = 10, 
-                     text = "Plot") 
-  
-# place the button  
-# in main window 
-plot_button.pack() 
-  
-# run the gui 
-window.mainloop() 
+import tkinter as tk
+import numpy as np
+import matplotlib
+matplotlib.use('TkAgg')
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg, NavigationToolbar2Tk)
+
+class DatavisApp:
+    def __init__(self, master):
+        self.master = master
+        master.title("Datavis App")
+        master.geometry("800x600")
+
+        # Create a container for the plots
+        self.plot_container = tk.Frame(master)
+        self.plot_container.pack(fill=tk.BOTH, expand=True)
+
+        # Create buttons to switch between plots
+        self.button_frame = tk.Frame(master)
+        self.button_frame.pack(side=tk.TOP, fill=tk.X)
+
+        self.scatter_button = tk.Button(self.button_frame, text="Scatter Plot", command=self.plot_scatter)
+        self.scatter_button.pack(side=tk.LEFT, padx=10, pady=10)
+
+        self.heatmap_button = tk.Button(self.button_frame, text="Heatmap", command=self.plot_heatmap)
+        self.heatmap_button.pack(side=tk.LEFT, padx=10, pady=10)
+
+        # Initialize the scatter plot
+        self.plot_scatter()
+
+    def plot_scatter(self):
+        # Clear the plot container
+        for widget in self.plot_container.winfo_children():
+            widget.destroy()
+
+        month = ["jan", "feb", "mar", "apr", "may", "jun", "jul"]
+        attendance = np.random.randint(50, 1000, 7)
+
+        fig, ax = plt.subplots(figsize=(8, 6))
+        ax.scatter(month, attendance)
+        ax.set_xticks(np.arange(len(month)), labels=month)
+        plt.setp(ax.get_xticklabels(), rotation=45, ha="right", rotation_mode="anchor")
+        ax.set_title("Attendance")
+
+        canvas = FigureCanvasTkAgg(fig, master=self.plot_container)
+        canvas.draw()
+        canvas.get_tk_widget().pack(fill=tk.BOTH, expand=True)
+
+        toolbar = NavigationToolbar2Tk(canvas, self.plot_container)
+        toolbar.update()
+        canvas.get_tk_widget().pack(fill=tk.BOTH, expand=True)
+
+    def plot_heatmap(self):
+        # Clear the plot container
+        for widget in self.plot_container.winfo_children():
+            widget.destroy()
+
+        month = ["jan", "feb", "mar", "apr", "may", "jun", "jul"]
+        schools = ["Jacobs Younguns", "Mafia Kidz.", "Lil Sheetos", "Soap droppers", "Mangetout's Mangled", "Stink gang", "Kidz."]
+        attendance = np.random.randint(50, 1000, [7, 7])
+
+        fig, ax = plt.subplots(figsize=(8, 6))
+        im = ax.imshow(attendance)
+
+        ax.set_xticks(np.arange(len(month)), labels=month)
+        ax.set_yticks(np.arange(len(schools)), labels=schools)
+        plt.setp(ax.get_xticklabels(), rotation=45, ha="right", rotation_mode="anchor")
+
+        for i in range(len(month)):
+            for j in range(len(schools)):
+                text = ax.text(j, i, attendance[i, j], ha="center", va="center", color="w")
+
+        ax.set_title("Attendance Heatmap")
+        fig.tight_layout()
+
+        canvas = FigureCanvasTkAgg(fig, master=self.plot_container)
+        canvas.draw()
+        canvas.get_tk_widget().pack(fill=tk.BOTH, expand=True)
+
+        toolbar = NavigationToolbar2Tk(canvas, self.plot_container)
+        toolbar.update()
+        canvas.get_tk_widget().pack(fill=tk.BOTH, expand=True)
+
+root = tk.Tk()
+app = DatavisApp(root)
+root.mainloop()
